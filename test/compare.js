c6d3ab2f6a577f91eb856ff85e10322527c6433b
egghead <thomas.omans@gmail.com>
2014-09-28 21:02:09 +0000

    more comparison tweaks :skull:

--- test/compare.js
+++ test/compare.js
@@ -6,8 +6,8 @@ Naive comparison of performance between lodash, mori, and ishtar
 Granted, the projects have different goals - still we don't want to be unnecessarily slow.
 */
 
-var x = 100000;
-console.log("Running 100k transformations each");
+var x = 1000000;
+console.log("Running 1mil transformations each");
 console.log("=================================");
 console.log();
 
@@ -36,7 +36,7 @@ I S H T A R ====================
 ============================= */
 var ishtar = require("../core");
 var istart = new Date();
-var xform = ishtar.compose(ishtar.drop(x / 4), ishtar.map(ishtar.dec), ishtar.filter(odd), ishtar.take(1));
+var xform = ishtar.compose(ishtar.map(ishtar.dec), ishtar.filter(odd), ishtar.drop(x / 4), ishtar.take(1));
 ishtar.transduce(xform, ishtar.conj, ishtar.Range(0, x+1)).first();
 var iend = new Date();
 console.log("ishtar:", iend - istart + "ms");
