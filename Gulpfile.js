5ed99c29f7868b9622506c3282c54c8a96d54ae2
Thomas Omans <thomas.omans@gmail.com>
2014-09-27 22:46:25 -0700

    adding browser build :speedboat:

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,25 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var webpack = require('gulp-webpack');
+var uglify = require('webpack').optimize.UglifyJsPlugin
+
+var package = require('./package.json');
+
+gulp.task('dist', function() {
+  return gulp.src('core.js')
+    .pipe(webpack({
+      output: {
+        library: "Ishtar",
+        filename: "ishtar-" + package.version + ".min.js"
+      },
+      plugins: [ new uglify() ]
+     }))
+    .pipe(gulp.dest('dist/'));
+});
 
 gulp.task('test', function (){
     return gulp.src('./test/*_test.js', {read: false})
         .pipe(mocha({reporter: 'nyan'}));
 });
 
-gulp.task('default', ['test']);
+gulp.task('default', ['test', 'dist']);
